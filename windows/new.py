49c3c8873fd8052c8e4376192303ea3a9e881434
张文喆 <942740938@qq.com>
2018-03-10 16:46:45 +0800

    一步到位，微信amr转wav

--- windows/new.py
+++ windows/new.py
@@ -10,6 +10,7 @@ file_in = file_in.strip('"')
 
 fs_in = open(file_in,"rb")
 path = os.path.split(__file__)
+print(path)
 os.chdir(path[0])
 fs_out = open('temp.amr',"wb")
 
@@ -19,14 +20,14 @@ if not fs_in.read(1) == b'#':
 fs_out.write(fs_in.read())
 fs_out.close()
 
-if os.path.exists("silk2pcm.exe"):
+if os.path.exists("silk2wav.exe"):
     print("waiting...")
 else:
     print("decoder not exist!")
     print(os.path.abspath(__file__))
     exit(1)
 
-os.system(".\silk2pcm.exe "+"temp.amr "+file_out)
+os.system(".\silk2wav.exe "+"temp.amr "+file_out)
 
 os.remove("temp.amr")
 
